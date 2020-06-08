d2b65576ee435ca28007f929ed1d67edb976502b
NewWebSouth <romainlavabre98@gmail.com>
2020-06-08 14:25:05 +0200

    Stable 2.17.1

--- App/config/components.php
+++ App/config/components.php
@@ -7,27 +7,27 @@ return [
     /**
      * Let an manager take care your peristence class, your only job is of optimize your SQL request
      */
-    \NoMess\Component\PersistsManager\PersistsManager::class     => false,
+    \NoMess\Components\PersistsManager\PersistsManager::class     => false,
 
     /**
      * Let an manager take care your data
      */
-    \NoMess\Component\DataManager\Database::class                => false,
+    \NoMess\Components\DataManager\Database::class                => false,
 
     /**
      * Dynamically create your rewrite rules during program execution
      */
-    \NoMess\Component\RuntimeRewriteUrl\RewriteRule::class       => false,
+    \NoMess\Components\RuntimeRewriteUrl\RewriteRule::class       => false,
 
     /**
      * Use a light persistence container
      */
-    \NoMess\Component\LightPersists\LightPersists::class         => false,
+    \NoMess\Components\LightPersists\LightPersists::class         => false,
 
     /**
      * Use a worker for asynchronous tasks
      */
-    \NoMess\Component\Worker\InteractiveRuntimeWorker::class     => false
+    \NoMess\Components\Worker\InteractiveRuntimeWorker::class     => false
 
 
 
