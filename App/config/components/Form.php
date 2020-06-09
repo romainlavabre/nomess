feb4c30cf8a511e26bb0da2e7604230c37444797
NewWebSouth <romainlavabre98@gmail.com>
2020-06-09 22:44:48 +0200

    DEV

---
+++ App/config/components/Form.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'bootstrap' => [
+        'form' => [
+            'method' => 'POST'
+        ],
+        'group' => [
+            'class' => 'form-group'
+        ],
+        'input' => [
+            'text' => [
+                'class' => 'form-control'
+            ],
+            'number' => [
+                'class' => 'form-control'
+            ],
+            'password' => [
+                'class' => 'form-control'
+            ],
+            'email' => [
+                'class' => 'form-control'
+            ],
+            'tel' => [
+                'class' => 'form-control'
+            ],
+            'file' => [
+                'class' => 'form-control-file'
+            ],
+            'checkbox', 'radio' => [
+                'class' => 'form-check-input'
+            ],
+            'submit' => [
+                'class' => 'btn btn-success'
+            ]
+        ],
+        'select' => [
+            'class' => 'btn btn-success'
+        ]
+    ]
+];
