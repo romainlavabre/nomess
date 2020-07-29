d6e890379de5d98ce194f4c285c8b5dd7376579b
NewWebSouth <romainlavabre98@gmail.com>
2020-07-29 21:11:17 +0200

    2.20.3

--- config/container.php
+++ config/container.php
@@ -2,16 +2,11 @@
 
 
 return [
-
-
-
-    /*
-     ====================================== INTERNAL SERVICE ======================================
-     */
-
-    \NoMess\Database\IPDOFactory::class                                     => \NoMess\Database\PDOFactory::class,
-    \Nomess\Components\EntityManager\EntityManagerInterface::class          => \Nomess\Components\EntityManager\EntityManager::class,
-    \Nomess\Container\ContainerInterface::class                             => \Nomess\Container\Container::class,
-    \NoMess\Components\ApplicationScope\ApplicationScopeInterface::class    => \Nomess\Components\ApplicationScope\ApplicationScope::class,
-    \NoMess\Components\LightPersists\LightPersistsInterface::class          => \Nomess\Components\LightPersists\LightPersists::class
+    \NoMess\Database\IPDOFactory::class                                  => \NoMess\Database\PDOFactory::class,
+    \Nomess\Components\EntityManager\EntityManagerInterface::class       => \Nomess\Components\EntityManager\EntityManager::class,
+    \Nomess\Components\EntityManager\Event\CreateEventInterface::class   => \Nomess\Components\EntityManager\Event\CreateEvent::class,
+    \Nomess\Components\EntityManager\TransactionSubjectInterface::class  => \Nomess\Components\EntityManager\EntityManager::class,
+    \Nomess\Container\ContainerInterface::class                          => \Nomess\Container\Container::class,
+    \NoMess\Components\ApplicationScope\ApplicationScopeInterface::class => \Nomess\Components\ApplicationScope\ApplicationScope::class,
+    \NoMess\Components\LightPersists\LightPersistsInterface::class       => \Nomess\Components\LightPersists\LightPersists::class,
 ];
