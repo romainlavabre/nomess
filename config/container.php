ee939cd49054a4c156879dc99978d8c8976ed3ca
NewWebSouth <romainlavabre98@gmail.com>
2020-08-20 12:02:04 +0200

    2.25.3

--- config/container.php
+++ config/container.php
@@ -7,6 +7,6 @@ return [
     \Nomess\Components\EntityManager\Event\CreateEventInterface::class   => \Nomess\Components\EntityManager\Event\CreateEvent::class,
     \Nomess\Components\EntityManager\TransactionSubjectInterface::class  => \Nomess\Components\EntityManager\EntityManager::class,
     \Nomess\Container\ContainerInterface::class                          => \Nomess\Container\Container::class,
-    \NoMess\Components\ApplicationScope\ApplicationScopeInterface::class => \Nomess\Components\ApplicationScope\ApplicationScope::class,
-    \NoMess\Components\LightPersists\LightPersistsInterface::class       => \Nomess\Components\LightPersists\LightPersists::class,
+    \Nomess\Components\ApplicationScope\ApplicationScopeInterface::class => \Nomess\Components\ApplicationScope\ApplicationScope::class,
+    \Nomess\Components\LightPersists\LightPersistsInterface::class       => \Nomess\Components\LightPersists\LightPersists::class,
 ];
