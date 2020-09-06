8d9ba3cde5ab32f1cc8c52f11aa4e46e0d4a82fb
NewWebSouth <romainlavabre98@gmail.com>
2020-09-06 17:02:46 +0200

    3.0.0

--- public/index.php
+++ public/index.php
@@ -1,18 +1,19 @@
 <?php
-declare(strict_types=1);
+declare( strict_types=1 );
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-ini_set("log_errors", "1");
-
-
-define('ROOT', str_replace('public/index.php', '', $_SERVER['SCRIPT_FILENAME']));
-define('NOMESS_CONTEXT', 'DEV');
-ini_set('error_log', ROOT .'var/log/error.log');
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'on' );
+ini_set( "log_errors", "1" );
 
 
-require (ROOT . 'vendor/autoload.php');
-require (ROOT . 'vendor/nomess/kernel/Exception/NomessException.php');
+define( 'ROOT', str_replace( 'public/index.php', '', $_SERVER['SCRIPT_FILENAME'] ) );
+define( 'NOMESS_CONTEXT', 'DEV' );
+ini_set( 'error_log', ROOT . 'var/log/error.log' );
 
-( new Nomess\Initiator\Initiator() )->initializer();
+
+require( ROOT . 'vendor/autoload.php' );
+require( ROOT . 'vendor/nomess/kernel/Exception/NomessException.php' );
+
+$response = ( new Nomess\Initiator\Initiator() )->initializer();
+$response->show();
 
